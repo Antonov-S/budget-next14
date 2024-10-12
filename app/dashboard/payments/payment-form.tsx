5b36d98df1e178f8be6835a6c4a5b3b304a6ac15
Antonov-S <anto1nov1@gmail.com>
2024-10-13 02:09:27 +0300

    Create/Edit Payment server action and make some changes on PaymentForm

--- app/dashboard/payments/payment-form.tsx
+++ app/dashboard/payments/payment-form.tsx
@@ -2,17 +2,12 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useAction } from "next-safe-action/hooks";
 import { Euro } from "lucide-react";
 
 import { PaymentSchema, zPaymentSchema } from "@/types/payments-schema";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { createPayment } from "@/server/actions/create-payment";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Form,
   FormControl,
@@ -21,195 +16,214 @@ import {
   FormLabel,
   FormMessage
 } from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from "@/components/ui/select";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter
+} from "@/components/ui/card";
 
 export default function PaymentForm() {
+  type PaymentType = "expense" | "income";
+
   const form = useForm<zPaymentSchema>({
     resolver: zodResolver(PaymentSchema),
     defaultValues: {
       title: "",
       description: "",
-      type: undefined,
+      type: "expense", //default value
       amount: 0
-      //   category: "",
-      //   editMode: false
-    }
+    },
+    mode: "onChange"
   });
+
+  const { execute, status } = useAction(createPayment, {
+    onSuccess: data => {
+      if (data.data?.success) {
+        console.log(data.data?.success);
+      }
+    },
+    onError: error => console.log(error)
+  });
+
+  async function onSubmit(values: zPaymentSchema) {
+    execute(values);
+  }
 
   return (
     <Form {...form}>
-      <form onSubmit={() => console.log("hey")} className="space-y-4">
-        <Tabs defaultValue="expense" className="w-full">
-          <TabsList>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <Tabs
+          defaultValue="expense"
+          className="w-full"
+          onValueChange={(value: string) => {
+            if (value === "expense" || value === "income") {
+              form.setValue("type", value as PaymentType);
+            }
+          }}
+        >
+          <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="expense">Expense</TabsTrigger>
             <TabsTrigger value="income">Income</TabsTrigger>
           </TabsList>
+
+          {/* EXPENSE TAB */}
           <TabsContent value="expense">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Rent" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Monthly rent for the garage"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            {/* <FormField
-              control={form.control}
-              name="category"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Type</FormLabel>
-                  <FormControl className="w-full">
-                    <Select>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a category" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Types</SelectLabel>
-                          <SelectItem value="expense">Expense</SelectItem>
-                          <SelectItem value="income">Income</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            /> */}
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Amount</FormLabel>
-                  <FormControl>
-                    <div className="flex items-center gap-2">
-                      <Euro size={36} className="p-2 bg-muted rounded-md" />
-                      <Input
-                        type="number"
-                        placeholder="Amount"
-                        {...field}
-                        step="0.1"
-                        min={0}
-                      />
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <Card>
+              <CardHeader>
+                <CardTitle>Expense</CardTitle>
+                <CardDescription>
+                  Fill out the details for your expense.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Rent" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Monthly rent for the garage"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Amount</FormLabel>
+                      <FormControl>
+                        <div className="flex items-center gap-2">
+                          <Euro size={36} className="p-2 bg-muted rounded-md" />
+                          <Input
+                            type="number"
+                            placeholder="Amount"
+                            {...field}
+                            step="0.1"
+                            min={0}
+                          />
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter>
+                <Button
+                  className="w-full"
+                  disabled={
+                    status === "executing" ||
+                    !form.formState.isValid ||
+                    !form.formState.isDirty
+                  }
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </CardFooter>
+            </Card>
           </TabsContent>
+
+          {/* INCOME TAB */}
           <TabsContent value="income">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Salary" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Monthly salary" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            {/* <FormField
-              control={form.control}
-              name="category"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Type</FormLabel>
-                  <FormControl className="w-full">
-                    <Select>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a category" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Types</SelectLabel>
-                          <SelectItem value="expense">Expense</SelectItem>
-                          <SelectItem value="income">Income</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            /> */}
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Amount</FormLabel>
-                  <FormControl>
-                    <div className="flex items-center gap-2">
-                      <Euro size={36} className="p-2 bg-muted rounded-md" />
-                      <Input
-                        type="number"
-                        placeholder="Amount"
-                        {...field}
-                        step="0.1"
-                        min={0}
-                      />
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <Card>
+              <CardHeader>
+                <CardTitle>Income</CardTitle>
+                <CardDescription>
+                  Fill out the details for your income.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Salary" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Monthly salary" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Amount</FormLabel>
+                      <FormControl>
+                        <div className="flex items-center gap-2">
+                          <Euro size={36} className="p-2 bg-muted rounded-md" />
+                          <Input
+                            type="number"
+                            placeholder="Amount"
+                            {...field}
+                            step="0.1"
+                            min={0}
+                          />
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter>
+                <Button
+                  className="w-full"
+                  disabled={
+                    status === "executing" ||
+                    !form.formState.isValid ||
+                    !form.formState.isDirty
+                  }
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </CardFooter>
+            </Card>
           </TabsContent>
         </Tabs>
-
-        <Button className="w-full" type="submit">
-          Submit
-        </Button>
       </form>
     </Form>
   );
---
+++ server/actions/create-payment.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { actionClient } from "@/lib/safe-action";
+import { eq } from "drizzle-orm";
+
+import { PaymentSchema } from "@/types/payments-schema";
+import { payments } from "../schema";
+import { db } from "..";
+
+export const createPayment = actionClient
+  .schema(PaymentSchema)
+  .action(async ({ parsedInput: { id, title, description, amount, type } }) => {
+    try {
+      //EDIT MODE IF ID EXIST
+      if (id) {
+        const currentPayment = await db.query.payments.findFirst({
+          where: eq(payments.id, id)
+        });
+        if (!currentPayment) return { error: "Payment not found" };
+        const editedPayment = await db
+          .update(payments)
+          .set({ description, amount, title, type })
+          .where(eq(payments.id, id))
+          .returning();
+        revalidatePath("/dashboard/payments");
+        return { success: `Product ${editedPayment[0].title} has been edited` };
+      }
+      //NEW PAYMENT
+      if (!id) {
+        const newPayment = await db
+          .insert(payments)
+          .values({ description, amount, title, type })
+          .returning();
+        revalidatePath("/dashboard/payments");
+        return { success: `Product ${newPayment[0].title} has been created` };
+      }
+    } catch (err) {
+      return { error: "Failed to create payment" };
+    }
+  });
